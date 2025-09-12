204936ae0c89d0a0152ff4985b8a01041757f7e7
MarvelBoy <98831547+MarvelBoy047@users.noreply.github.com>
2025-09-12 08:08:41 +0530

    Delete OSS_UI directory

--- OSS_UI/src/lib/db/migrate.ts
+++
@@ -1,5 +0,0 @@
-import db from './';
-import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
-import path from 'path';
-
-migrate(db, { migrationsFolder: path.join(process.cwd(), 'drizzle') });
--- OSS_UI/src/lib/providers/server.ts
+++
@@ -1,7 +0,0 @@
-// src/lib/providers/server.ts
-import 'server-only';
-import { getAvailableChatModelProviders } from './index';
-
-export async function getAvailableChatModelProvidersServer() {
-  return await getAvailableChatModelProviders();
-}
